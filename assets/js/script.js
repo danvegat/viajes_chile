2f79110450be077a0108f077faf809735303f35d
Daniela Vega <d.vega.tirado@gmail.com>
2022-08-08 03:09:38 -0400

    tooltips en Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,17 @@
   
     y > 400 ? $("nav").addClass("nav-opacidad") : $("nav").removeClass("nav-opacidad");
   });
+
+   /*Tooltips*/
+
+$(function () {
+  $('[data-bs-toggle="tooltip"]').tooltip();
+});
+
+  $("#enviarFormulario").click(function () {
+    alert("El formulario fue enviado correctamente");
+  });
+
+  $(".card-title").click(function () {
+    $(".card-text").toggle();
+  });
